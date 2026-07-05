89194bc81f85ac3216bccf1636fc8ba19dd82f7c
Bagus Indriyanto <bagusindriyanto07@gmail.com>
2026-07-05 12:15:43 +0700

    feat: add google search in general mode

--- src/features/ai/chat.ts
+++ src/features/ai/chat.ts
@@ -59,6 +59,7 @@ async function generalChat(conversation: Content[], isThinking?: boolean) {
         // thinkingLevel: isThinking ? ThinkingLevel.HIGH : ThinkingLevel.MINIMAL,
         // thinkingBudget: isThinking ? -1 : 0,
       },
+      tools: [{ googleSearch: {} }],
       systemInstruction: `
       [Role]
       Kamu adalah Finabot seorang financial advisor, yang punya gaya bahasa sopan dan suka
